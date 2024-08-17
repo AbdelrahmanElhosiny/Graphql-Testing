55bc0de0ca5f2eac1ef5751349d100e938dc9970
AbdelrahmanElhosiny <abdohosini13@gmail.com>
2024-08-17 05:29:46 +0300

    add `query`

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import styled from "styled-components";
 import "./App.css";
 
+const QUERY = `query {
+  country(code: "EG") {
+    code
+    emoji
+  }
+}`;
+
+const fetchGraphql = async () => {
+  const response = await fetch("https://countries.trevorblades.com/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: QUERY,
+    }),
+  });
+
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+
+fetchGraphql();
+
 function App() {
-  return <></>;
+  return <Holder></Holder>;
 }
+
+const Holder = styled.div``;
 
 export default App;
